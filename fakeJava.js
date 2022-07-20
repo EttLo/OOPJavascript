9b17437c3c49848bd118a40a483f6bdbe7a6de9a
EttLo <101204865+EttLo@users.noreply.github.com>
2022-07-20 17:05:28 +0200

    table to show the employee data

--- fakeJava.js
+++ fakeJava.js
@@ -1,12 +1,12 @@
 // Java copy-cat
-class Employee{
-    constructor(name, surname, sex){
+class Employee {
+    constructor(name, surname, sex) {
         this.name = name;
         this.surname = surname;
         this.sex = sex;
     }
 
-    sayHello(){
+    sayHello() {
         console.log(`Hi from ${this.name}`);
     }
 }
@@ -14,14 +14,14 @@ let emp = new Employee("Ciccio", "Pasticcio", "Male");
 console.log(emp.surname);
 emp.sayHello();
 
-class Manager extends Employee{
-    constructor(name, surname, sex, yearsOfExperience, bonusSalary){
+class Manager extends Employee {
+    constructor(name, surname, sex, yearsOfExperience, bonusSalary) {
         super(name, surname, sex);
         this.yearsOfExperience = yearsOfExperience;
         this.bonusSalary = bonusSalary;
     }
 
-    fire(){
+    fire() {
         console.log(this.yearsOfExperience);
         console.log(this.name);
         console.log("You're fired! Bye bye");
@@ -33,20 +33,36 @@ manager.sayHello();
 let div = document.getElementById("myDiv");
 div.innerHTML = "<div id = 'subDiv'> <p> 'Ciao Mondo' </p> </div>";
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-let div2 = document.querySelector("#myDiv"); 
+let div2 = document.querySelector("#myDiv");
 let divs = document.querySelectorAll(".myDiv"); //JQuery copy-cat
 
 //callback, it will be called later on
-function loadDocument(){
+function loadDocument() {
     let ajax = new XMLHttpRequest(); //Async javascript and XML
-    ajax.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            document.querySelector('#data').innerHTML = this.responseText;
+    ajax.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            let k = this.responseText.replace("[{", "{");
+            k = k.replace("}]", "}");
+            let splittedData = this.responseText.split("},");
+            // document.querySelector('#employeeTableData').innerHTML = this.responseText;
+            splittedData.forEach(row => {
+                document.querySelector('#employeeTableRows').innerHTML = "<tr id='employeeTableRow'></tr>";
+                let column = row.split(",")
+                column.forEach(datapoint => {
+                    datapoint = datapoint.replace("{", "");
+                    datapoint = datapoint.replace("}", "");
+                    document.querySelector('#employeeTableRow').innerHTML = "<td>" + datapoint + "</td>";
+                });
+            });
         }
     };
     ajax.open("GET", "data.json");
     ajax.send();
 }
-loadDocument();
+
+let b = getElementById("getEmployeeTableData")
+b.onclick = loadDocument();
+
 
 
